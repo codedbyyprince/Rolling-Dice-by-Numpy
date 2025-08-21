c2d1e8777df8a3aa4f574d015671fe558eb3e8af
prince <princenagda35@gmail.com>
2025-08-21 20:39:30 +0530

    trying to make this dice roller next level

---
+++ functions .py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def dice_roll():
+    return np.random.randint(1, 7) 
+
+def reverse_dice():
+    return np.random.randint(-7, 0)
+
--- main.py
+++ main.py
@@ -1,10 +1,2 @@
+from flask import Flask, render_template, request
 import numpy as np
-
-def dice_roll():
-    return np.random.randint(1, 7) 
-
-def reverse_dice():
-    return np.random.randint(-7, 0)
-
-dice_rolls = [dice_roll() for _ in range(10)]
-print(dice_rolls)
