14ef6cc1c40ab2533f008cf71f562389484e497c
prince <princenagda35@gmail.com>
2025-08-21 21:55:56 +0530

    dice roller completed one more feature remaining to add

--- main.py
+++ main.py
@@ -4,6 +4,15 @@ from modules.dice import dice_roll, reverse_dice, points
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
+    result = None
+    if request.method == "POST":
+        # only roll when button is clicked
+        result = dice_roll()  
+    
+    return render_template('index.html', result=result)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
